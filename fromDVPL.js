035e02ac4f34a8306eaad753efc8588c1bbb5b27
Maddoxkkm <maddox.kkm@gmail.com>
2020-01-04 00:18:31 +1100

    remove old files in favour for rewrite and global installation

--- fromDVPL.js
+++
@@ -1,3 +0,0 @@
-const reFS = require('./fileSystem.js');
-
-reFS.recursionOnFolders(reFS.decompress(),'./fromDVPL','output_fromDVPL');
--- toDVPL.js
+++
@@ -1,3 +0,0 @@
-const reFS = require('./fileSystem.js');
-
-reFS.recursionOnFolders(reFS.compress,'./toDVPL','output_toDVPL');
